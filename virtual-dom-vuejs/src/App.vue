98aab7c6b19902a47122d822f09cd4b861f24dc8
Nicolas Glassey <nicolas.glassey@eduvaud.ch>
2025-02-12 15:49:21 +0100

    feat(virtual-dom-vue): starting point

---
+++ virtual-dom-vuejs/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app">
+    <h1>Vue Counter App</h1>
+    //TODO
+  </div>
+</template>
+
+<script>
+import Counter from "./components/Counter.vue";
+
+export default {
+  components: {
+    Counter,
+  },
+};
+</script>
+
+<style scoped>
+.app {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
---
+++ virtual-dom-vuejs/src/components/Counter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="counter">
+    <h2>Counter: {{ count }}</h2>
+    //TODO
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    //TODO
+
+    const increment = () => {
+      count.value++;
+    };
+
+    return { count, increment };
+  },
+};
+</script>
+
+<style scoped>
+.counter {
+  margin-top: 20px;
+}
+button {
+  padding: 10px;
+  font-size: 16px;
+  cursor: pointer;
+}
+</style>
