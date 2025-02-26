f4799f1379aa083d8d41d09cfc79c384e870649e
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2025-02-26 15:47:56 +0100

    feat: add DataBinding component and add logic

---
+++ data-binding-vue/src/components/DataBinding.vue
@@ -0,0 +1,19 @@
+<script setup>
+const name = 'John Doe'
+const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4']
+
+</script>
+
+<template>
+  <h1>Vue Data Binding</h1>
+  <div id="name-container">
+    <p>{{ name }}</p>
+  </div>
+
+  <div id="items-container">
+    <ul id="items-list">
+      <li v-for="item in items" :key="item">{{ item }}</li>
+    </ul>
+  </div>
+
+</template>
